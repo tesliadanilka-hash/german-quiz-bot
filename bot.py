1f119ca40a9d162772aa821faaacb2998c96924a
tesliadanilka-hash <tesliadanilka@gmail.com>
2025-11-28 12:15:46 +0100

    bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ TOKEN = "8583421204:AAHB_2Y8RjDQHDQLcqDLJkYfiP6oBqq3SyE"
 
 # ID администратора, которому будут приходить запросы на доступ
 # Узнать можно, например, через @userinfobot
-ADMIN_ID = 123456789  # ЗАМЕНИ НА СВОЙ TELEGRAM ID
+ADMIN_ID = @danyloteslia  # ЗАМЕНИ НА СВОЙ TELEGRAM ID
 
 # Файл со списком пользователей, у которых есть доступ
 ALLOWED_USERS_FILE = "allowed_users.txt"
@@ -2121,3 +2121,4 @@ async def main() -> None:
 
 if __name__ == "__main__":
     asyncio.run(main())
+
