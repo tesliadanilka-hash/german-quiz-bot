67f557199ce496c372df9809b7b5c4a2f88bd687
tesliadanilka-hash <tesliadanilka@gmail.com>
2025-12-17 12:56:44 +0100

    loader.py

--- loader.py
+++ loader.py
@@ -4,10 +4,7 @@ from aiogram.client.default import DefaultBotProperties
 from config import TOKEN
 
 def create_bot() -> Bot:
-    return Bot(
-        token=TOKEN,
-        default=DefaultBotProperties(parse_mode="Markdown"),
-    )
+    return Bot(token=TOKEN, default=DefaultBotProperties(parse_mode="Markdown"))
 
 def create_dispatcher() -> Dispatcher:
     return Dispatcher()
