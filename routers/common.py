3cc698d65a7fa04ea2189d7f5da8f920b0b30929
tesliadanilka-hash <tesliadanilka@gmail.com>
2025-12-17 11:05:49 +0100

    common.py

--- routers/common.py
+++ routers/common.py
@@ -1,16 +1,16 @@
 from aiogram import Router, F
 from aiogram.types import CallbackQuery
 
-from config import ADMIN_ID
 from keyboards.main import build_main_menu_keyboard
 from services.access import has_access
 
 router = Router()
 
+
 @router.callback_query(F.data.in_({"back_main", "main_menu"}))
 async def cb_back_main(callback: CallbackQuery) -> None:
     uid = callback.from_user.id
-    if not has_access(uid, ADMIN_ID):
+    if not has_access(uid):
         await callback.answer("Нет доступа.", show_alert=True)
         return
 
